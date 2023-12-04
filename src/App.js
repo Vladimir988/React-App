0fb56f4d57a74ede0ed566a4738e8c74af1d6e73
Vladimir988 <optimist0706@gmail.com>
2023-12-04 16:43:12 +0200

    setPosts

--- src/App.js
+++ src/App.js
@@ -12,12 +12,21 @@ function App() {
   ]);
 
   const [title, setTitle] = useState();
-  const bodyInputRef = useRef();
+  const [body, setBody] = useState();
 
   const addNewPost = (e) => {
     e.preventDefault();
-    console.log(title);
-    console.log(bodyInputRef.current.value);
+    const newPost = {
+      id: Date.now(),
+      title,
+      body
+    }
+    setPosts([
+      ...posts,
+      newPost
+    ]);
+    setTitle('');
+    setBody('');
   }
 
   return (
@@ -30,7 +39,8 @@ function App() {
           placeholder='Title'
         />
         <MyInput
-          ref={bodyInputRef}
+          value={body}
+          onChange={e => setBody(e.target.value)}
           type="text"
           placeholder='Description'
         />

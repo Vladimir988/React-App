2ce2ddef45fb017ec1861c48cf8774f7c118ac95
Vladimir988 <optimist0706@gmail.com>
2023-12-05 13:54:23 +0200

    new component MySelect + sort fn

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, {useState} from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from "./components/PostForm";
+import MySelect from "./components/Ui/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([
-    {id: 1, title: 'Javascript 1', body: 'Description 1'},
-    {id: 2, title: 'Javascript 2', body: 'Description 2'},
-    {id: 3, title: 'Javascript 3', body: 'Description 3'},
+    {id: 1, title: 'B Javascript 1', body: 'C Description 1'},
+    {id: 2, title: 'A Javascript 2', body: 'B Description 2'},
+    {id: 3, title: 'C Javascript 3', body: 'A Description 3'},
   ]);
+
+  const [selectedSort, setSelectedSort] = useState('');
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -18,10 +21,28 @@ function App() {
     setPosts(posts.filter(p => p.id !== postId));
   };
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+  };
+
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList remove={removePost} posts={posts} title='Posts list 1:' />
+      <hr style={{margin:'20px 0'}} />
+      <MySelect
+        value={selectedSort}
+        onChange={sortPosts}
+        defaultValue='Filter by'
+        options={[
+          {value: 'title', name: 'Filter by name'},
+          {value: 'body', name: 'Filter by description'}
+        ]}
+      />
+      {posts.length
+        ? <PostList remove={removePost} posts={posts} title='Posts list 1:' />
+        : <h1 style={{textAlign:'center'}}>Posts not found!</h1>
+      }
     </div>
   );
 }

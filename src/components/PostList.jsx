37173d805707275c32aa3c3fd108c744f717e4f8
Vladimir988 <optimist0706@gmail.com>
2023-12-12 22:36:12 +0200

    npm install react-transition-group --save

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -11,6 +11,7 @@ const PostList = ({posts, title, remove}) => {
   return (
     <div>
       <h1 style={{textAlign:'center', marginTop: '15px'}}>{title}</h1>
+      <TransitionGroup></TransitionGroup>
       {posts.map((post, index) =>
         <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>
       )}
